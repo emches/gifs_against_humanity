92044b847bb38c5a561815afda2793f17533e97e
emilyches <emilychesler@gmail.com>
2016-01-25 09:49:48 -0500

    removed shuffle

--- server/seeds/questions.js
+++ server/seeds/questions.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 
-var questions = [{"title": "When someone texts back k", "used": false },
+var questions = [
+{"title": "When someone texts back k", "used": false },
+{"title": "When you\'re running late but find out REACTO\'s cancelled", "used": false},
+{"title": "When they say they\'re Good but they\'re Shafiq", "used": false},
+{"title": "When you have to present after Ches and Nick's presentation", "used": false},
+{"title": "When Netflix asks if you\'re still there", "used": false},
+{"title": "When Andrew overhears \"But TSwift writes her own songs!\"", "used": false},
 {"title": "When you\'re about to leave but your song comes on", "used": false},
-{"title": "When Netflix asks if youre still there", "used": false},
 {"title": "When you think you posted something hilarious but no one likes it...", "used": false},
 {"title": "When your phone autocorrects to \'duck\'", "used": false},
 {"title": "When you flip the pillow over to the cold side", "used": false},
@@ -13,13 +18,10 @@ var questions = [{"title": "When someone texts back k", "used": false },
 {"title": "When your StackStore finally updates the damn Cart", "used": false},
 {"title": "When you\'re trying to find the validation error in your seed file", "used": false},
 {"title": "When the new Juniors ask you code things", "used": false},
-{"title": "When they say they\'re Good but they\'re Shafiq", "used": false},
 {"title": "When you get Merlin for the first time", "used": false},
 {"title": "When you\'re at Fullstack and your friends ask if you can hang midweek", "used": false},
 {"title": "When spend an hour debugging and realize it was just a spelling error", "used": false},
 {"title": "When Sean hear\'s someone say \"for loop\"", "used": false},
-{"title": "When Andrew overhears someone say \"Well at least Taylor Swift writes her own songs\"", "used": false},
-{"title": "When you have to present after Ches and Nick's presentation", "used": false}
 
 
 
