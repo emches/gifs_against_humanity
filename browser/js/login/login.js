96871a9168dc36ff018a732b347b89d1cbf6d0db
Nick <npalenchar@gmail.com>
2016-01-24 19:20:41 -0500

    better login validation

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -8,16 +8,22 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('LoginCtrl', function($scope, Socket, $window, AuthService, $state, UserFactory, GifFactory, QuestionFactory) {
+app.controller('LoginCtrl', function ($scope, Socket, $window, AuthService, $state, UserFactory, GifFactory, QuestionFactory) {
 
-    Socket.on('connect', function(){
+    Socket.on('connect', function () {
         console.log("I HAVE CONNECTED")
 
     });
-
+    $scope.header = "GIFS AGAINST HUMANITY"
     $scope.login = {};
     $scope.error = null;
     $scope.userCount = 0;
+    $scope.playerMinimum = 3;
+    $scope.submitted = false;
+    $scope.submitBtnText = "ADD USER";
+    $scope.getRemaining = function () {
+        return $scope.playerMinimum - $scope.userCount - 1;
+    };
 
 
     $scope.sendLogin = function (loginInfo) {
@@ -34,59 +40,71 @@ app.controller('LoginCtrl', function($scope, Socket, $window, AuthService, $stat
     $scope.allPlayers = [];
     var me;
 
-    $scope.addUser = function(){
-       // $state.go('home', { newUser: $scope.newUser});
-       console.log("all", $scope.allPlayers);
-       if ($scope.allPlayers.length >2){ return $window.alert("ROOM FULL SORRY!!!"); }
-       if ($scope.allPlayers.indexOf($scope.newUser) > -1) { return $window.alert("USER ALREADY EXISTS"); }
+    $scope.addUser = function () {
+        if(/[^\w\d\s]+/g.test($scope.newUser)){
+            $scope.newUser = "";
+            return $window.alert("ALPHA-NUMERIC-SPACE CHARACTERS ONLY!");
+        }
+        if ($scope.allPlayers.length > 2) {
+            return $window.alert("ROOM FULL SORRY!!!");
+        }
+        if ($scope.allPlayers.indexOf($scope.newUser) > -1) {
+            return $window.alert("USER ALREADY EXISTS");
+        }
 
+        $scope.submitted = true;
         UserFactory.addUser($scope.newUser)
-            .then(function(user){
-                console.log("got user back", user);
+            .then(function (user) {
                 user.currentStatus = "PLAYER";
-               // $scope.allPlayers.push(user)
+                // $scope.allPlayers.push(user)
                 //$scope.currentUser = user
-               // console.log("new current", $scope.currentUser)
-                $scope.userCount+=1;
+                // console.log("new current", $scope.currentUser)
+                $scope.userCount += 1;
                 $scope.allPlayers.push(user);
                 me = user;
                 console.log("new count", $scope.userCount);
-               // if ($scope.userCount===6){ $scope.roomReady= true}
+                // if ($scope.userCount===6){ $scope.roomReady= true}
 
                 Socket.emit('newPlayer', $scope.allPlayers, $scope.userCount);
 
 
             })
 
-   };
+    };
 
-      $scope.joinRoom = function(){
-          console.log("current me", me);
-          var gifDeck;
-          GifFactory.constructApiDeck()
-              .then(deck => gifDeck = deck)
-              .then(() => QuestionFactory.constructQuestionDeck())
-              .then(questionDeck => GifFactory.saveConstructedDecks(questionDeck, gifDeck))
-              .then(savedDeck => Socket.emit('joinRoom', savedDeck._id));
-      };
+    $scope.joinRoom = function () {
+        console.log("current me", me);
+        var gifDeck;
+        GifFactory.constructApiDeck()
+            .then(deck => gifDeck = deck)
+            .then(() => QuestionFactory.constructQuestionDeck())
+            .then(questionDeck => GifFactory.saveConstructedDecks(questionDeck, gifDeck))
+            .then(savedDeck => Socket.emit('joinRoom', savedDeck._id));
+    };
 
-      Socket.on('gameStart', function(deckId){
+    Socket.on('gameStart', function (deckId) {
 
-              console.log("NEW VARS", deckId);
-                console.log("PLAYERS", $scope.allPlayers);
-              $state.go('home', {
-                  allPlayers: $scope.allPlayers,
-                  me: me,
-                  deckId: deckId
-              });
-          });
+        console.log("NEW VARS", deckId);
+        console.log("PLAYERS", $scope.allPlayers);
+        $state.go('home', {
+            allPlayers: $scope.allPlayers,
+            me: me,
+            deckId: deckId
+        });
+    });
 
-      Socket.on('newPlayerFromServer', function(allPlayers, userCount){
-            $scope.allPlayers = allPlayers;
-            $scope.userCount = userCount;
-            console.log("allPlayers", allPlayers);
-            console.log("count", userCount);
-            $scope.$digest()
+    Socket.on('newPlayer', function (allPlayers, userCount) {
+        var plural = $scope.getRemaining() === 1 ? "s." : ".";
+        if ($scope.submitted) {
+            $scope.submitBtnText = "Waiting for others..."
+        }
+        $scope.allPlayers = allPlayers;
+        $scope.userCount = userCount;
+        if ($scope.userCount >= $scope.playerMinimum) $scope.header = "READY TO PLAY";
+        console.log("allPlayers", allPlayers);
+        console.log("count", userCount);
+        $scope.newUser = "";
+        $scope.$digest()
 
-      })
+    })
 });
